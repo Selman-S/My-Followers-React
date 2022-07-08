27bb80029d40aa6d68d64a91126a6e6287d29819
selman <gunduzgece546@gmail.com>
2022-07-08 23:20:14 +0300

    about finished

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -11,8 +11,8 @@ const LoginForm = () => {
     window.location.href="/home"
   }
   return (
-    <div> <Form onSubmit={handleSubmit}>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
+    <div> <Form onSubmit={handleSubmit} className="forms">
+    <Form.Group className="mt-3 " controlId="formBasicEmail">
       <Form.Label>Email address</Form.Label>
       <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e)=> setEmail(e.target.value)} required/>
       <Form.Text className="text-muted">
@@ -20,12 +20,12 @@ const LoginForm = () => {
       </Form.Text>
     </Form.Group>
 
-    <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Group className="mb-3 " controlId="formBasicPassword">
       <Form.Label>Password</Form.Label>
       <Form.Control type="password" placeholder="Password" required />
     </Form.Group>
    
-    <Button variant="primary" type="submit">
+    <Button variant="primary" className="mx-auto" type="submit">
       Submit
     </Button>
   </Form></div>
